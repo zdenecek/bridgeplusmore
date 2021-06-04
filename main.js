6bbef6501f935d82e4de6c384d0acd6da8e0b6df
zdenecek <zdnek.tomis@gmail.com>
2021-06-04 21:50:21 +0200

    added dotenv

--- main.js
+++ main.js
@@ -1,21 +1,16 @@
-
-async function login(browser) {
-    console.log('logging in...');
-    return 5;
-}
+require('dotenv').config();
+const puppeteer = require('puppeteer');
+const querystring = require('querystring');
 
 console.log('The script is running...');
 
-const puppeteer = require('puppeteer');
-
 (async () => {
-    const browser = await puppeteer.launch({headless: false});
-    
+    const browser = await puppeteer.launch({ headless: false });
+
     await (async (browser) => {
         const page = await browser.newPage();
 
-        await page.screenshot({path: "screenshots/test.jpg"});
-        await login(browser);
+        await login(page);
 
         console.log('The script is concluded...');
     })(browser).finally(async () => {
@@ -25,3 +20,44 @@ const puppeteer = require('puppeteer');
     process.exit(0);
 })();
 
+
+async function login(page) {
+    console.log('logging in...');
+    await page.goto('https://bridgeplusserver.com/');
+
+
+    await page.type("#email", process.env.BM_EMAIL);
+    await page.type("#pwd", process.env.BM_PASSWORD)
+
+    await page.click("#terms");
+
+    await Promise.all([
+        page.keyboard.press('Enter'),
+        page.waitForNavigation()
+    ])
+    await page.screenshot({ path: "screenshots/test.jpg" });
+}
+
+async function createNewBoard(page) {
+    await page.setRequestInterception(true);
+
+    page.once("request", interceptedRequest => {
+        interceptedRequest.continue({
+            method: "POST",
+            postData: " ",
+            headers: {
+                ...interceptedRequest.headers(),
+                "Content-Type": "application/x-www-form-urlencoded"
+            }
+        });
+    });
+
+    const response = await page.goto("https://postman-echo.com/post");
+
+    console.log({
+        url: response.url(),
+        statusCode: response.status(),
+        body: await response.text()
+    });
+
+}
